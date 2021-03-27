589b9e55ab4dcf0b4b3114e7e6ff4c83044ae1d1
Shahid Bahadur <shahidbahadur1998@gmail.com>
2021-03-27 15:58:45 +0500

    second commit

---
+++ index.js
@@ -0,0 +1,18 @@
+$(".slider").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
